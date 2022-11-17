34c03bb4d0e7ff88411a5bc0600976db7a64685f
Dan Trofimov <daniil.trofimov@sbermarket.ru>
2022-11-17 04:44:25 +0300

    update statoscope.config.js

--- statoscope.config.js
+++ statoscope.config.js
@@ -3,7 +3,6 @@ module.exports = {
         plugins: ['@statoscope/webpack'],
         rules: {
             '@statoscope/webpack/build-time-limits': ['error', 5000],
-            '@statoscope/webpack/restricted-packages': ['error', ['bn.js', 'browserify-crypto']],
             '@statoscope/webpack/entry-download-size-limits': ['error', { global: { maxSize: 3 * 1024 * 100 } }],
             '@statoscope/webpack/no-packages-dups': ['error'],
         }
